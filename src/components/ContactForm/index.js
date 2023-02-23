925a2566439b834c877b34f0229790519f8c0f9d
Paulo Ricardo Pinto Godoy <paulo.godoy@compasso.com.br>
2023-02-23 10:34:48 -0300

    Ajuste formulario de contato

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,27 +1,98 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import { ContactInfo, Label, Title, ContactWrapper } from "./styles";
+import { Container, Form, Button } from "react-bootstrap";
+import { Label, Title, ContactWrapper, SubmitButton } from "./styles";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
-function ContactForm() {
+const schema = Yup.object().shape({
+  email: Yup.string().email("E-mail inválido").required("Campo obrigatório"),
+  message: Yup.string().required("Campo obrigatório"),
+  phone: Yup.string().required("Campo obrigatório"),
+  address: Yup.string().required("Campo obrigatório"),
+});
+
+const ContactForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
+
   return (
     <ContactWrapper id="contact">
       <Container>
         <Title>Entre em contato conosco</Title>
-        <ContactInfo>
-          <Label>Telefone:</Label>
-          <span>(11) 1234-5678</span>
-        </ContactInfo>
-        <ContactInfo>
-          <Label>E-mail:</Label>
-          <span>contato@petdoddy.com</span>
-        </ContactInfo>
-        <ContactInfo>
-          <Label>Endereço:</Label>
-          <span>Rua dos Pets, 123, São Paulo - SP</span>
-        </ContactInfo>
+        <Formik
+          initialValues={{ email: "", message: "", phone: "", address: "" }}
+          validationSchema={schema}
+          onSubmit={handleSubmit}
+        >
+          {({ handleSubmit, handleChange, values, touched, errors }) => (
+            <Form noValidate onSubmit={handleSubmit}>
+              <Form.Group controlId="formEmail">
+                <Label>E-mail:</Label>
+                <Form.Control
+                  type="email"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  isInvalid={touched.email && !!errors.email}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="formPhone">
+                <Label>Telefone:</Label>
+                <Form.Control
+                  type="text"
+                  name="phone"
+                  value={values.phone}
+                  onChange={handleChange}
+                  isInvalid={touched.phone && !!errors.phone}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.phone}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="formAddress">
+                <Label>Endereço:</Label>
+                <Form.Control
+                  type="text"
+                  name="address"
+                  value={values.address}
+                  onChange={handleChange}
+                  isInvalid={touched.address && !!errors.address}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.address}
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group controlId="formMessage">
+                <Label>Mensagem:</Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  name="message"
+                  value={values.message}
+                  onChange={handleChange}
+                  isInvalid={touched.message && !!errors.message}
+                />
+                <Form.Control.Feedback type="invalid">
+                  {errors.message}
+                </Form.Control.Feedback>
+              </Form.Group>
+              <SubmitButton>
+                <button type="submit">Enviar</button>
+              </SubmitButton>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </ContactWrapper>
   );
-}
+};
 
 export default ContactForm;
--- src/components/ContactForm/styles.js
+++ src/components/ContactForm/styles.js
@@ -1,24 +1,96 @@
 import styled from "styled-components";
-
-export const ContactWrapper = styled.div`
-  padding-top: 90px;
-`;
-
-export const Title = styled.h2`
-  font-size: 2.5rem;
-  font-weight: 600;
-  margin-bottom: 2rem;
-  text-align: center;
-`;
+import { Container, Form, Button } from "react-bootstrap";
 
 export const ContactInfo = styled.div`
   display: flex;
-  align-items: center;
-  margin-bottom: 1.5rem;
+  justify-content: center;
+  margin-bottom: 30px;
 `;
 
-export const Label = styled.span`
-  font-size: 1.2rem;
-  font-weight: 600;
-  margin-right: 0.5rem;
+export const Label = styled.label`
+  margin-right: 10px;
+  width: 70px;
+  text-align: right;
+`;
+
+export const Title = styled.h2`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+export const ContactWrapper = styled.div`
+  background-color: #f7f7f7;
+  padding: 60px 0;
+`;
+
+export const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledForm = styled(Form)`
+  max-width: 500px;
+  width: 100%;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export const FormGroup = styled(Form.Group)`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const Feedback = styled(Form.Control.Feedback)`
+  font-size: 0.8rem;
+  margin-top: 5px;
+`;
+
+export const StyledFormControl = styled(Form.Control)`
+  font-size: 1rem;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #f7f7f7;
+  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.1);
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 2px #007bff;
+  }
+
+  &:disabled {
+    background-color: #eee;
+  }
+`;
+
+export const StyledTextArea = styled(StyledFormControl)`
+  resize: none;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+  margin-top: 15px;
+  background: #f7f7f7;
+  border: none;
+  color: white;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  font-weight: 700;
+  border-radius: 5px;
+  outline: none;
+
+  button:hover {
+    background: #ddd;
+    cursor: pointer;
+  }
+
+  button {
+    width: 100%;
+  }
 `;
